5bcd2a09421497498117111b194739bf5e32ad3d
Rodriguez2 <Juan.Rodriguez2@sage.com>
2019-03-22 11:19:28 +0000

    creating randomizer that adds characters, moved Generators to its own file

---
+++ GitHubWebUserFinder.Tests/Models/Generators.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Linq;
+using FsCheck;
+
+namespace GitHubWebUserFinder.Tests.Models
+{
+	public static class Generators
+	{
+		private static string _allowedCharacters = "*()&%$£@<>";
+		private static string _alphabet = "abcdefghijklmnopqrstuvwxyz";
+		private static string _numbers = "0123456789";
+
+		public static Gen<int> RandomNumber(int min, int max)
+		{
+			return Gen.Choose(min, max);
+		}
+
+		public static Arbitrary<char[]> LettersGenerator()
+		{
+			return Gen.ArrayOf(RandomNumber(5, 100).Sample(20, 1).First(), Gen.Elements($"{_alphabet}{_alphabet.ToUpper()}".ToCharArray())).ToArbitrary();
+		}
+
+		public static Arbitrary<char[]> AlphanumericGenerator()
+		{
+			return Gen.ArrayOf(RandomNumber(5, 100).Sample(20, 1).First(), Gen.Elements($"{_alphabet}{_alphabet.ToUpper()}{_numbers}".ToCharArray())).ToArbitrary();
+		}
+
+		public static Arbitrary<char[]> NotEmptyStringWithAllowedCharactersGenerator()
+		{
+			return Gen.ArrayOf(RandomNumber(5, 100).Sample(20, 1).First(), Gen.Elements($"{_alphabet}{_alphabet.ToUpper()}{_numbers}{_allowedCharacters}".ToCharArray())).ToArbitrary();
+		}
+
+		public static Gen<JObject> GitHubUserGenerator = from name in LettersGenerator().Generator
+			from userLocation in AlphanumericGenerator().Generator
+			from alias in AlphanumericGenerator().Generator
+			from avatarurl in NotEmptyStringWithAllowedCharactersGenerator().Generator
+			select JObject.FromObject(new
+			{
+				name = new string(name),
+				location = new string(userLocation),
+				login = new string(alias),
+				avatar_url = new string(avatarurl)
+			});
+	}
+}
--- GitHubWebUserFinder.Tests/Models/GitHubUserTests.cs
+++ GitHubWebUserFinder.Tests/Models/GitHubUserTests.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json.Linq;
-using System.Linq;
-using GitHubWebUserFinder.Models;
+﻿using GitHubWebUserFinder.Models;
 using Newtonsoft.Json;
 using FsCheck;
 using NUnit.Framework.Internal;
@@ -62,44 +60,5 @@ namespace GitHubWebUserFinder.Tests.Models
 				Assert.AreEqual(expectedAvatarUrl, result.AvatarUrl);
 			}).QuickCheck();
 		}
-	}
-
-	public static class Generators
-	{
-		private static string _alphabet = "abcdefghijklmnopqrstuvwxyz";
-		private static string _numbers = "0123456789";
-
-		private static Gen<NonEmptyString> NotEmptyString()
-		{
-			return Arb.Default.NonEmptyString().Generator;
-		}
-
-		public static Gen<int> RandomNumber(int min, int max)
-		{
-			return Gen.Choose(min, max);
-		}
-
-		public static Arbitrary<char[]> LettersGenerator()
-		{
-			return Gen.ArrayOf(RandomNumber(5, 100).Sample(20, 1).First(), Gen.Elements($"{_alphabet}{_alphabet.ToUpper()}".ToCharArray())).ToArbitrary();
-		}
-
-		public static Arbitrary<char[]> AlphanumericGenerator()
-		{
-			return Gen.ArrayOf(RandomNumber(5, 100).Sample(20, 1).First(), Gen.Elements($"{_alphabet}{_alphabet.ToUpper()}{_numbers}".ToCharArray())).ToArbitrary();
-		}
-
-		public static Gen<JObject> GitHubUserGenerator = from name in LettersGenerator().Generator
-			from userLocation in AlphanumericGenerator().Generator
-			from alias in AlphanumericGenerator().Generator
-			from avatarurl in AlphanumericGenerator().Generator
-			from car in AlphanumericGenerator().Generator
-			select JObject.FromObject(new
-			{
-				name = new string(name),
-				location = new string(userLocation),
-				login = new string(alias),
-				avatar_url = new string(avatarurl)
-			});
 	}
 }
