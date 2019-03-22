52becda6c2122b7c3eb88afd98664185ff63a1d7
Rodriguez2 <Juan.Rodriguez2@sage.com>
2019-03-22 09:08:14 +0000

    adding custom routing

--- GitHubWebUserFinder/App_Start/RouteConfig.cs
+++ GitHubWebUserFinder/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace GitHubWebUserFinder
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+			routes.MapMvcAttributeRoutes();
+
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
