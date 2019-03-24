983389574593d0dcd01134bbac7a15664ce96be7
Rodriguez2 <Juan.Rodriguez2@sage.com>
2019-03-24 20:12:07 +0000

    missing test

---
+++ GitHubWebUserFinder.Tests/Models/UserSearchTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using GitHubWebUserFinder.Models;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace GitHubWebUserFinder.Tests.Models
+{
+	[TestFixture]
+	class UserSearchTests
+	{
+		[Test]
+		public void UserSearch_WithNoCriteria_IsInvalid()
+		{
+			UserSearch testSearch = new UserSearch();
+
+			var results = ValidateModel(testSearch);
+
+			Assert.AreEqual(1, results.Count);
+			Assert.AreEqual("The Criteria field is required.", results.First().ErrorMessage);
+		}
+
+		[Test]
+		public void UserSearch_WithUserNameWithCharactersNotAcceptedByGitHub_IsInvalid()
+		{
+			UserSearch testSearch = new UserSearch
+			{
+				Criteria = "john.doe"
+			};
+
+			var results = ValidateModel(testSearch);
+
+			Assert.AreEqual(1, results.Count);
+			Assert.AreEqual("Search may only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen", results.First().ErrorMessage);
+		}
+
+		[Test]
+		public void UserSearch_Valid_WillNotReturnErrors()
+		{
+			UserSearch testSearch = new UserSearch
+			{
+				Criteria = "versatil56"
+			};
+
+			var results = ValidateModel(testSearch);
+
+			Assert.AreEqual(0,results.Count);
+		}
+
+		private static IList<ValidationResult> ValidateModel(object modelToValidate)
+		{
+			List<ValidationResult> validationResult = new List<ValidationResult>();
+			ValidationContext validationContext = new ValidationContext(modelToValidate, null, null);
+			Validator.TryValidateObject(modelToValidate, validationContext, validationResult, true);
+
+			return validationResult;
+		}
+	}
+}
